cb7c27ce5daafb83f0cbed2c8040e0d227cde0a1
gsabbih6 <gsabbih5@gmail.comm>
2021-07-12 09:45:29 -0400

    comitted second

--- src/main/java/com/sabbih/demo/Stylzcrawlerv1Application.java
+++ src/main/java/com/sabbih/demo/Stylzcrawlerv1Application.java
@@ -2,7 +2,12 @@ package com.sabbih.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 @EnableCassandraRepositories
